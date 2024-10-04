f6b0326c578e688026e2943815706a89b8dcae51
Nikhil Sharma <nikhilsharma.is22@bmsce.ac.in>
2024-10-05 00:56:15 +0530

    Initial commit

---
+++ server/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import http from 'http';
+import cors from 'cors';
+import { Server } from 'socket.io';
+const app = express();
+
+app.use(cors());
+const server = http.createServer(app);
+
+const io=new Server(server,{
+    cors:{
+        origin: "http://localhost:3000",
+        methods: ["GET","POST"],
+    },
+})
+
+io.on("connection",(socket)=>{
+    console.log(`user connected ${socket.id}`);
+    socket.on("join_room",(data)=>{
+        socket.join(data);
+        console.log(`User with id :${socket.id} joined room ${data}`)
+    })
+
+    socket.on("send_message",(data)=>{
+        socket.to(data.room).emit("receive_message",data);
+    })
+
+    socket.on("disconnect",()=>{
+        console.log("user disconnect",socket.io);
+    });
+});
+
+server.listen(8000,()=>{
+    console.log(`server is running on port 8000`);
+})
